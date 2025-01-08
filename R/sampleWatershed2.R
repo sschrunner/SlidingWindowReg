24a3fa86300a441abc34bcb079af9c8e72f721d8
Schrunner Stefan <schrunner@fh-kaernten.at>
2025-01-08 15:30:14 +0100

    added second sample dataset

---
+++ R/sampleWatershed2.R
@@ -0,0 +1,12 @@
+#' Sample watershed 2
+#'
+#' A dataset containing a time series of daily rainfall and measured water runoff in a gauged watershed over 39 years
+#'
+#' @format A data frame with 14153 rows and 3 columns:
+#' \describe{
+#'   \item{date}{date variable (daily measurements) between January 01, 1980 and September 30, 2018}
+#'   \item{rain}{daily cumulated rainfall}
+#'   \item{gauge}{daily cumulated, gauged watershed runoff}
+#' }
+#' @source processed by HydroGeoScience for Watershed Management Research group at the University of British Columbia, Vancouver, Canada
+"sampleWatershed2"
